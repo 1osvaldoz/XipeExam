e6ad9a4f07e3c3717312a0acb07ac311dfd635a9
Manuel Osvaldo Monzón Pérez <osvaldo.monzon@lennken.com>
2020-10-13 10:04:46 -0700

    .

--- ExamenXipe/ExamenXipe/wwwroot/js/Views/Index.js
+++ ExamenXipe/ExamenXipe/wwwroot/js/Views/Index.js
@@ -37,51 +37,61 @@ var indexFunction = {
             });
          
         return false;
+    },
+    loadFields: function () {
+        $("#contactHead").empty();
+        $("#contactHead").append(objForm.form_heading);
+        var html = "";
+        objForm.fields.map(function (item, i) {
+            $("#formContainer").empty();
+
+            var validations = "";
+            Object.keys(item.input.validations).map(function (field, i) {
+                if (field == "required" && item.input.validations[field] == false) {
+                    validations += "";
+                }
+                else {
+
+                    validations += field + `="` + item.input.validations[field] + `" `;
+
+                }
+            })
+
+            switch (item.input.type) {
+                case "textarea":
+                    html += ` 
+                            <div class="col-md-12">
+
+                                <div class="md-form">
+                                    <textarea data-model="mail" data-prop="` + item.input.name + `" type="text" id="txtArea` + i + `" name="` + item.name + `"   class="form-control md-textarea" ` + validations + `></textarea>
+                                    <label for="` + item.input.name + `">` + item.label.text + `</label>
+                                </div>
+                        </div>`
+                    break;
+                default:
+                    html += `<div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input  data-model="mail" data-prop="` + item.input.name + `" type="` + item.input.type + `" id="txt` + i + `" name="` + item.input.name + `" class="form-control" ` + validations + `>
+                                <label for="` + item.input.name + `" class="">` + item.label.text + `</label>
+                            </div>
+                        </div>`
+                    break;
+            }
+        });
+        html += ` 
+                            <div class="col-md-12">
+
+                                <div class="md-form">
+                                   <button type="submit" class="btn btn-danger">`+ objForm.submit_button.text + `</button>
+                                </div>
+                        </div>`
+
+        $("#formContainer").append(html);
+        loadData()
     }
 }
 
 $(function () {
-    $("#contactHead").empty();
-    $("#contactHead").append(objForm.form_heading);
-    var html = "";
-    objForm.fields.map(function (item, i) {
-        $("#formContainer").empty();
-
-        var validations = "";
-        Object.keys(item.input.validations).map(function (field, i) {
-            validations += field+`="` + item.input.validations[field]+`" `;
-        })
-
-        switch (item.input.type) {
-            case "textarea":
-                html += ` 
-                            <div class="col-md-12">
-
-                                <div class="md-form">
-                                    <textarea data-model="mail" data-prop="` + item.input.name + `" type="text" id="txtArea`+ i + `" name="` + item.name + `"   class="form-control md-textarea" ` + validations +`></textarea>
-                                    <label for="` + item.input.name + `">`+item.label.text+`</label>
-                                </div>
-                        </div>`
-                break;
-            default:
-                html += `<div class="col-md-6">
-                            <div class="md-form mb-0">
-                                <input  data-model="mail" data-prop="` + item.input.name + `" type="`+ item.input.type + `" id="txt` + i + `" name="` + item.input.name + `" class="form-control" ` + validations +`>
-                                <label for="` + item.input.name + `" class="">`+ item.label.text +`</label>
-                            </div>
-                        </div>`
-                break;
-        }
-    });
-  html += ` 
-                            <div class="col-md-12">
-
-                                <div class="md-form">
-                                   <button type="submit" class="btn btn-danger">`+ objForm.submit_button.text+`</button>
-                                </div>
-                        </div>`
-
-    $("#formContainer").append(html);
-    loadData()
+    indexFunction.loadFields();
 
 })
